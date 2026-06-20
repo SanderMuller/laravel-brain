3e71bfbdaaf80c3f3cefb7ccbbdd02777c46ee1b
MrMarchOne <mrmarchone@gmail.com>
2026-06-20 23:52:54 +0300

    Fix Security Issues

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,12 +8,12 @@
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=JetBrains+Mono:wght@400;500;600&display=swap" rel="stylesheet" />
-    <script type="module" crossorigin src="/_laravel-brain/assets/index-DcehtwT1.js"></script>
-    <link rel="modulepreload" crossorigin href="/_laravel-brain/assets/rolldown-runtime-CkqCuyE9.js">
-    <link rel="modulepreload" crossorigin href="/_laravel-brain/assets/vendor-B5MdJxr-.js">
-    <link rel="modulepreload" crossorigin href="/_laravel-brain/assets/vendor-react-emvhMHcg.js">
-    <link rel="modulepreload" crossorigin href="/_laravel-brain/assets/vendor-d3-BerEHusR.js">
-    <link rel="modulepreload" crossorigin href="/_laravel-brain/assets/vendor-utils-DjfM5PxJ.js">
+    <script type="module" crossorigin src="/_laravel-brain/assets/index-Ce-6gnQZ.js"></script>
+    <link rel="modulepreload" crossorigin href="/_laravel-brain/assets/rolldown-runtime-BHe-jwch.js">
+    <link rel="modulepreload" crossorigin href="/_laravel-brain/assets/vendor-CsjAK7B8.js">
+    <link rel="modulepreload" crossorigin href="/_laravel-brain/assets/vendor-react-CNktMmef.js">
+    <link rel="modulepreload" crossorigin href="/_laravel-brain/assets/vendor-d3-DThTr3c3.js">
+    <link rel="modulepreload" crossorigin href="/_laravel-brain/assets/vendor-utils-D7YtnGoz.js">
     <link rel="stylesheet" crossorigin href="/_laravel-brain/assets/index-CHLc6t41.css">
   </head>
   <body>
